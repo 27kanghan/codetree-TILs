b235b2f92d715ca871f329dae798019c9cdd2b77
강한 <58686114+27kanghan@users.noreply.github.com>
2024-01-04 19:08:17 +0900

    Title: 별표 출력하기 5, Time: 142ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240104/별표 출력하기 5/print-star-5.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+
+        int n = Integer.parseInt(st.nextToken());
+
+
+        for(int i = n; i > 0; i--) {
+            for (int j = i; j > 0; j--) {
+                for (int k = i; k > 0; k--) {
+                    sb.append("*");
+                }
+                sb.append(" ");
+            }
+            sb.append("\n");
+        }
+
+
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+
+    }
+}
