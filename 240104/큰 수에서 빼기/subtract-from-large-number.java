977f38e564f20ddf884b04ce4fed11d2f6f8da3b
강한 <58686114+27kanghan@users.noreply.github.com>
2024-01-04 16:03:13 +0900

    Title: 큰 수에서 빼기, Time: 93ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240104/큰 수에서 빼기/subtract-from-large-number.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int num1 = Integer.parseInt(st.nextToken());
+        int num2 = Integer.parseInt(st.nextToken());
+
+        if(num1 > num2){
+            sb.append(num1-num2);
+        }else{
+            sb.append(num2-num1);
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
