489bf39ea427653a9e376e36063d8b255be22513
강한 <58686114+27kanghan@users.noreply.github.com>
2024-01-05 19:03:43 +0900

    Title: 정수 복사, Time: 86ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240105/정수 복사/copy-integer.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+
+        int a = 3;
+        int b = 4;
+        
+        b = a;
+        
+        sb.append(a);
+        sb.append(" ");
+        sb.append(b);
+        sb.append("\n");
+        sb.append(a*b);
+
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+
+    }
+}
