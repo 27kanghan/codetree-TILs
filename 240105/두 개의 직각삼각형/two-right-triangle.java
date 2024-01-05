92b2304fbbf28e564ec82776bed9a05c9dafd124
강한 <58686114+27kanghan@users.noreply.github.com>
2024-01-05 11:33:23 +0900

    Title: 두 개의 직각삼각형, Time: 97ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240105/두 개의 직각삼각형/two-right-triangle.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+
+        int n = Integer.parseInt(st.nextToken());
+
+
+        for(int i = 0; i < n; i++){
+            for(int j = i; j < n; j++){
+                sb.append("*");
+            }
+            for(int j = 0; j < i; j++){
+                sb.append(" ");
+            }
+            for(int j = 0; j < i; j++){
+                sb.append(" ");
+            }
+            for(int j = i; j < n; j++){
+                sb.append("*");
+            }
+
+            sb.append("\n");
+        }
+
+
+
+
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+
+    }
+}
