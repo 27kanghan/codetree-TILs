418fd40282e609852d40ee971c9e331a854b30bf
강한 <58686114+27kanghan@users.noreply.github.com>
2024-01-05 19:02:26 +0900

    Title: 문자 변경하기, Time: 85ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240105/문자 변경하기/change-charater.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+
+        String a = "c";
+        a = "T";
+
+
+
+
+
+        sb.append(a);
+
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+
+    }
+}
