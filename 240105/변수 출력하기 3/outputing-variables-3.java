faabf235e6b8999a7d23267db7cf052c81bdf423
강한 <58686114+27kanghan@users.noreply.github.com>
2024-01-05 18:54:29 +0900

    Title: 변수 출력하기 3, Time: 93ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240105/변수 출력하기 3/outputing-variables-3.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        
+        int a = 1;
+        int b = 2;
+        String c = "C";
+        String arrow = "->";
+        
+        sb.append(a);
+        sb.append(arrow);
+        sb.append(b);
+        sb.append(arrow);
+        sb.append(c);
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+
+    }
+}
