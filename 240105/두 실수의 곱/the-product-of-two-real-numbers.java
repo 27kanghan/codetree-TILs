e5dc7130e6aeb1553e8aa860c81780bdf67f21c7
강한 <58686114+27kanghan@users.noreply.github.com>
2024-01-05 19:01:13 +0900

    Title: 두 실수의 곱, Time: 115ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240105/두 실수의 곱/the-product-of-two-real-numbers.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+
+        double a = 5.26;
+        double b = 8.27;
+
+        double ab = a*b;
+
+
+
+        sb.append(String.format("%.3f",ab));
+
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+
+    }
+}
