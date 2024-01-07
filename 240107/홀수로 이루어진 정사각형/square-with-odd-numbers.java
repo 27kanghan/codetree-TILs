a501d80a1fd1101367b3fc457a031adf6bd0d180
강한 <58686114+27kanghan@users.noreply.github.com>
2024-01-07 21:50:21 +0900

    Title: 홀수로 이루어진 정사각형, Time: 100ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240107/홀수로 이루어진 정사각형/square-with-odd-numbers.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+
+        int n = Integer.parseInt(st.nextToken());
+        int num = 11;
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n*2; j+=2){
+                sb.append(num+j);
+                sb.append(" ");
+
+            }
+            num += 2;
+            sb.append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+
+    }
+}
