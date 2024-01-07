fab4b6d7a8479cd83df3f88d7633c68d9c0e2d10
강한 <58686114+27kanghan@users.noreply.github.com>
2024-01-07 21:59:10 +0900

    Title: 모양대로 숫자 출력하기 2, Time: 85ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240107/모양대로 숫자 출력하기 2/print-out-numbers-in-specific-shape-2.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+
+        int n = Integer.parseInt(st.nextToken());
+        int cnt = 2;
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                sb.append(cnt);
+                sb.append(" ");
+                if(cnt >= 8){
+                    cnt = 2;
+                }else {
+                    cnt += 2;
+                }
+            }
+            sb.append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+
+    }
+}
