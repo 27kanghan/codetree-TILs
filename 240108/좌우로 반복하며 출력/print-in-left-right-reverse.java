cbfa4d443bcd85b87d7d1e83c309b371b51cd687
강한 <58686114+27kanghan@users.noreply.github.com>
2024-01-08 15:28:06 +0900

    Title: 좌우로 반복하며 출력, Time: 92ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240108/좌우로 반복하며 출력/print-in-left-right-reverse.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+
+        int n = Integer.parseInt(st.nextToken());
+        for(int i = 0; i < n; i++){
+            if(i % 2 != 0){
+                for(int j = n; j>0; j--){
+                    sb.append(j);
+                }
+            }else{
+                for(int j = 0; j < n; j++){
+                    sb.append(j+1);
+                }
+            }
+            sb.append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+
+    }
+}
