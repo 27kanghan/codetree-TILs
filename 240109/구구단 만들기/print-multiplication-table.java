fc2e608f2d7aca5f0507903a080ba9b6059490c2
강한 <58686114+27kanghan@users.noreply.github.com>
2024-01-09 17:04:02 +0900

    Title: 구구단 만들기, Time: 114ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240109/구구단 만들기/print-multiplication-table.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        for(int i = 1; i <= 9; i++){
+            for(int j = m; j >= n; j-=2){
+                sb.append(j);
+                sb.append(" * ");
+                sb.append(i);
+                sb.append(" = ");
+                sb.append(i * j);
+                if(j != n){
+                    sb.append(" / ");
+                }
+
+            }
+            sb.append("\n");
+        }
+
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+
+    }
+}
