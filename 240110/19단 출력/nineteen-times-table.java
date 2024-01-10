cf29f082265248e22aa05820fede571bba7e60d0
강한 <58686114+27kanghan@users.noreply.github.com>
2024-01-10 16:06:32 +0900

    Title: 19단 출력, Time: 88ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240110/19단 출력/nineteen-times-table.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+
+//        int n = Integer.parseInt(st.nextToken());
+        for(int i = 1; i <= 19; i++){
+            for(int j = 1; j <= 19; j ++){
+                sb.append(i);
+                sb.append(" * ");
+                sb.append(j);
+                sb.append(" = ");
+                sb.append(i*j);
+                if(j == 19){
+                    sb.append("\n");
+                }else if(j % 2 == 0){
+                    sb.append("\n");
+                }else{
+                    sb.append(" / ");
+                }
+            }
+        }
+
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+
+    }
+}
