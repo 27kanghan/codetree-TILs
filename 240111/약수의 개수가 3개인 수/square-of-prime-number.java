4ebe2290ef85e9fd0aae2943d1e8b4ce07f245b1
강한 <58686114+27kanghan@users.noreply.github.com>
2024-01-11 17:01:28 +0900

    Title: 약수의 개수가 3개인 수, Time: 111ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240111/약수의 개수가 3개인 수/square-of-prime-number.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int res = 0;
+        for (int i = n; i <= m; i++) {
+
+            int sum = 0;
+            int cnt = 0;
+            for (int j = 1; j < i; j++) {
+                if (i % j == 0) {
+                    cnt++;
+                }
+            }
+            
+            if(cnt == 3){
+                res++;
+            }
+
+            
+
+
+        }
+
+        sb.append(res);
+
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+
+    }
+}
