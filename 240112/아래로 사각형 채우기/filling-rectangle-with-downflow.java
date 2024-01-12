d8a0b511de83cf1d457315b8ccb8a8e338d9c367
강한 <58686114+27kanghan@users.noreply.github.com>
2024-01-12 17:04:19 +0900

    Title: 아래로 사각형 채우기, Time: 107ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240112/아래로 사각형 채우기/filling-rectangle-with-downflow.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+
+
+
+        int arr[][] = new int[n][n];
+
+
+        for (int i = 1; i <= n; i++) {
+            int cnt = i;
+            for (int j = 0; j < n; j++) {
+                arr[i-1][j] = cnt;
+                cnt+=5;
+            }
+        }
+        
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                sb.append(arr[i][j] + " ");
+            }
+            
+            sb.append("\n");
+        }
+
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+
+    }
+}
