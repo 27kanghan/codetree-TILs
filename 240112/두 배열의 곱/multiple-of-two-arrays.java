3b7922e126167391ac65196d9da5d6eeb695c725
강한 <58686114+27kanghan@users.noreply.github.com>
2024-01-12 16:28:44 +0900

    Title: 두 배열의 곱, Time: 93ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240112/두 배열의 곱/multiple-of-two-arrays.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+
+        int arr[][] = new int[3][3];
+        int arr2[][] = new int[3][3];
+
+
+        for (int i = 0; i < 3; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 3; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        String str = br.readLine();
+
+        for (int i = 0; i < 3; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 3; j++) {
+                arr2[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                sb.append(arr[i][j] * arr2[i][j] + " ");
+            }
+
+            sb.append("\n");
+        }
+
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+
+    }
+}
