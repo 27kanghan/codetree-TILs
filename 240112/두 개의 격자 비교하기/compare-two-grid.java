14457255805cfefb238b1fa78123fd7c3b4fe2b0
강한 <58686114+27kanghan@users.noreply.github.com>
2024-01-12 16:34:26 +0900

    Title: 두 개의 격자 비교하기, Time: 109ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240112/두 개의 격자 비교하기/compare-two-grid.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+
+        int arr[][] = new int[n][m];
+        int arr2[][] = new int[n][m];
+
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                arr2[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if(arr[i][j] != arr2[i][j]){
+                    sb.append(1);
+                }else{
+                    sb.append(0);
+                }
+                sb.append(" ");
+
+
+            }
+
+            sb.append("\n");
+        }
+
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+
+    }
+}
