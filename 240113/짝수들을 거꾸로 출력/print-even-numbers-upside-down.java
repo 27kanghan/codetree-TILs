0487ecb366c81e3d89c9955fec5bfe3d9e679115
강한 <58686114+27kanghan@users.noreply.github.com>
2024-01-13 18:06:06 +0900

    Title: 짝수들을 거꾸로 출력, Time: 93ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240113/짝수들을 거꾸로 출력/print-even-numbers-upside-down.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+
+        int arr[] = new int [n];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        for(int i = n-1; i >= 0 ; i--){
+            if(arr[i] % 2 == 0){
+                sb.append(arr[i]);
+                sb.append(" ");
+            }
+        }
+
+
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+
+    }
+}
