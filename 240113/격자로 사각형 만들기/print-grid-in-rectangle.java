b7f174fbccd6a8a797f77dd9059aa06fa705b495
강한 <58686114+27kanghan@users.noreply.github.com>
2024-01-13 18:02:41 +0900

    Title: 격자로 사각형 만들기, Time: 106ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240113/격자로 사각형 만들기/print-grid-in-rectangle.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+
+        int arr[][] = new int[n][n];
+
+
+        for(int i = 0; i < n; i++){
+                arr[i][0] = arr[0][i] = 1;
+            }
+
+        for(int i = 1; i < n; i ++) {
+            for(int j = 1; j < n; j++){
+                arr[i][j] = arr[i-1][j] + arr[i-1][j-1] + arr[i][j-1];
+            }
+        }
+
+        for(int i = 0 ; i < n; i++){
+            for(int j = 0 ; j < n; j++){
+                sb.append(arr[i][j] + " ");
+            }
+            sb.append("\n");
+        }
+
+
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+
+    }
+}
