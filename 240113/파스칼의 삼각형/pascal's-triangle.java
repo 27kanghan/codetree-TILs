5501a15e1f0356a10de6a21db38e968f716514b4
강한 <58686114+27kanghan@users.noreply.github.com>
2024-01-13 18:01:11 +0900

    Title: 파스칼의 삼각형, Time: 90ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240113/파스칼의 삼각형/pascal's-triangle.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+
+        int arr[][] = new int[n][n];
+
+
+        for(int i = 0; i < n; i++){
+                arr[i][0] = arr[i][i] = 1;
+            }
+
+        for(int i = 2; i < n; i ++) {
+            for(int j = 1; j <= i-1; j++){
+                arr[i][j] = arr[i-1][j-1] + arr[i-1][j];
+
+            }
+        }
+
+        for(int i = 0 ; i < n; i++){
+            for(int j = 0 ; j <= i; j++){
+                sb.append(arr[i][j] + " ");
+            }
+            sb.append("\n");
+        }
+
+
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+
+    }
+}
