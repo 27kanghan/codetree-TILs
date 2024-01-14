4ce718a3d45ba4816082e753e16ab7713ca7b4b6
강한 <58686114+27kanghan@users.noreply.github.com>
2024-01-14 18:51:16 +0900

    Title: 공백을 기준으로 출력, Time: 99ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240114/공백을 기준으로 출력/output-based-on-space.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+
+        String str = "";
+
+
+        for(int i = 0; i < 2; i++){
+            str += br.readLine();
+        }
+
+        sb.append(str.replace(" ",""));
+
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+
+    }
+}
