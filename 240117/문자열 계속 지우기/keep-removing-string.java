382f5d3b500f6b3af3a2e9d83227e8315ed50947
강한 <58686114+27kanghan@users.noreply.github.com>
2024-01-17 17:50:03 +0900

    Title: 문자열 계속 지우기, Time: 106ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240117/문자열 계속 지우기/keep-removing-string.java
@@ -0,0 +1,44 @@
+import java.io.*;
+
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+
+        String str = br.readLine();
+        String a = br.readLine();
+        int totalLen = str.length();
+        int len = a.length();
+
+        while (true) {
+            if (str.contains(a)) {
+                for (int i = 0; i < totalLen - len+1; i++) {
+                    if (str.substring(i, i + len).equals(a)) {
+                        str = str.substring(0, i) + str.substring(i + len);
+                        
+                        totalLen = str.length();
+                        
+                    }
+                }
+                
+            }else {
+                break;
+            }
+
+        }
+
+        sb.append(str);
+
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+
+    }
+
+}
