b6f4ee75b6cc200a6853312b5c105f5a1ea7f6d9
강한 <58686114+27kanghan@users.noreply.github.com>
2024-01-17 15:20:53 +0900

    Title: 문자 제거하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240117/문자 제거하기/del-char.java
@@ -0,0 +1,47 @@
+import java.io.*;
+
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+
+        String str = br.readLine();
+
+        while (true) {
+
+            if (str.length() == 1) {
+                break;
+            }
+
+            st = new StringTokenizer(br.readLine());
+
+            int num = Integer.parseInt(st.nextToken());
+
+
+            if (num > str.length()) {
+                str = str.substring(0, str.length() - 1);
+            }else if(num == 0){
+                str = str.substring(1, str.length());
+            }
+            else {
+                str = str.substring(0, num) + str.substring(num+1);
+            }
+            
+            sb.append(str);
+            sb.append("\n");
+
+        }
+
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+
+    }
+
+}
