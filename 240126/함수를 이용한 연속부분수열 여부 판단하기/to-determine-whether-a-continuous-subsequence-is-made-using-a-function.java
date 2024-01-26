f55f9d38193a18ffb53398259e9807b52421af16
강한 <58686114+27kanghan@users.noreply.github.com>
2024-01-26 17:38:24 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 181ms, Memory: 8MB, Status: Passed - CodeTree

--- 240126/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.java
+++ 240126/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.java
@@ -59,7 +59,7 @@ public class Main {
 
         boolean flags = false;
 
-        for (int i = 0; i < arr1.length - arr2.length; i++) {
+        for (int i = 0; i < arr1.length - arr2.length + 1; i++) {
             int cnt = 0;
             for (int j = 0; j < arr2.length; j++) {
                 if (arr1[i + j] == arr2[j]) {
