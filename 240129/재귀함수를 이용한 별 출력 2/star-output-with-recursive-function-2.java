3257ded3c3ff56268136d33eccf965f8063f4a57
강한 <58686114+27kanghan@users.noreply.github.com>
2024-01-29 18:12:20 +0900

    Title: 재귀함수를 이용한 별 출력 2, Time: 104ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240129/재귀함수를 이용한 별 출력 2/star-output-with-recursive-function-2.java
@@ -0,0 +1,51 @@
+import java.io.*;
+
+import java.util.*;
+
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    static int arr[] ;
+
+    public static void main(String[] args) throws IOException {
+
+        int N = Integer.parseInt(br.readLine());
+
+        print(N);
+
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+
+    }
+
+
+    private static void print(int n) {
+
+        if(n == 0){
+            return;
+        }
+
+        for(int i = 0; i < n; i++){
+            sb.append("*");
+            sb.append(" ");
+        }
+        sb.append("\n");
+        print(n-1);
+        for(int i = 0; i < n; i++){
+            sb.append("*");
+            sb.append(" ");
+        }
+        sb.append("\n");
+
+
+
+    }
+
+
+}
