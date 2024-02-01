d78e9b6e84e39f52de990b13994d441e40bcf26a
강한 <58686114+27kanghan@users.noreply.github.com>
2024-02-01 18:47:48 +0900

    Title: 사는 지역, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240201/사는 지역/where-live.java
@@ -0,0 +1,79 @@
+import java.io.*;
+
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    static class CodeTree {
+
+        String name;
+        String phoneNumber;
+        String area;
+
+        public CodeTree(String name, String phoneNumber, String area) {
+            this.name = name;
+            this.phoneNumber = phoneNumber;
+            this.area = area;
+        }
+
+        @Override
+        public String toString() {
+            return "CodeTree{" +
+                    "name='" + name + '\'' +
+                    ", phoneNumber='" + phoneNumber + '\'' +
+                    ", area='" + area + '\'' +
+                    '}';
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        int N = Integer.parseInt(br.readLine());
+
+        CodeTree codeTree[] = new CodeTree[N];
+        String name1[] = new String[N];
+
+
+        for(int i = 0; i < 3; i++) {
+            st = new StringTokenizer(br.readLine());
+            String name = st.nextToken();
+            String phoneNumber = st.nextToken();
+            String area = st.nextToken();
+            name1[i] = name;
+            codeTree[i] = new CodeTree(name, phoneNumber, area);
+        }
+
+
+        Arrays.sort(name1);
+
+
+        int idx = 0;
+
+        for(int i = 0; i < 3; i++){
+            if(name1[2].equals(codeTree[i].name)){
+                idx = i;
+            }
+        }
+
+        System.out.println("name " + codeTree[idx].name);
+        System.out.println("addr " + codeTree[idx].phoneNumber);
+        System.out.println("city " + codeTree[idx].area);
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
