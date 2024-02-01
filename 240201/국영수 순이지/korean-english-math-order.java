97a64c418f063f1117872aaecd4bfd29368150a0
강한 <58686114+27kanghan@users.noreply.github.com>
2024-02-01 19:55:05 +0900

    Title: 국영수 순이지, Time: 153ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240201/국영수 순이지/korean-english-math-order.java
@@ -0,0 +1,70 @@
+import java.io.*;
+
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    static class CodeTree implements Comparable<CodeTree>{
+
+        String name;
+        int kor;
+        int eng;
+        int math;
+
+        public CodeTree(String name, int kor, int eng, int math) {
+            this.name = name;
+            this.kor = kor;
+            this.eng = eng;
+            this.math = math;
+        }
+
+        @Override
+        public int compareTo(CodeTree o) {
+            if(this.kor == o.kor){
+                if(this.eng == o.eng){
+                    return o.math - this.math;
+                }
+                return o.eng - this.eng;
+            }
+            return o.kor - this.kor;
+        }
+
+        @Override
+        public String toString() {
+            return name + " " + kor + " " + eng + " " + math;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        int N = Integer.parseInt(br.readLine());
+
+        CodeTree codeTree[] = new CodeTree[N];
+
+        for(int i = 0; i<N; i++){
+            st = new StringTokenizer(br.readLine());
+            String name = st.nextToken();
+            int kor = Integer.parseInt(st.nextToken());
+            int eng = Integer.parseInt(st.nextToken());
+            int math = Integer.parseInt(st.nextToken());
+
+            codeTree[i] = new CodeTree(name,kor,eng,math);
+        }
+
+        Arrays.sort(codeTree);
+
+
+        for(int i = 0; i < N; i++){
+            System.out.println(codeTree[i].toString());
+        }
+
+
+
+    }
+
+
+}
