18b21aa653c0273e98f36b442e25228ee7f717e8
강한 <58686114+27kanghan@users.noreply.github.com>
2024-02-04 20:17:58 +0900

    Title: 신기한 타일 뒤집기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240204/신기한 타일 뒤집기/strange-flipping-tiles.java
@@ -0,0 +1,63 @@
+import java.io.*;
+
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+
+    public static void main(String[] args) throws IOException {
+
+        int K = 1000;
+        int arr[] = new int[K * 2 + 1];
+
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int num = Integer.parseInt(st.nextToken());
+            String color = st.nextToken();
+
+            while(num-- > 0){
+                if(color.equals("R")){
+                    arr[K] = 1;
+                    
+                    if(num > 0)K++;
+                }
+
+                if(color.equals("L")){
+                    arr[K] = 2;
+                    
+                    if(num > 0) K--;
+                }
+            }
+
+
+        }
+
+        int whiteCnt = 0;
+        int blackCnt = 0;
+
+        for(int i = 0; i < 2001; i++){
+            if(arr[i] == 1){
+                blackCnt++;
+            }else if(arr[i] == 2){
+                whiteCnt++;
+            }
+
+        }
+
+        sb.append(whiteCnt + " " + blackCnt);
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+
+
+    }
+
+
+
+}
