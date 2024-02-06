4574d8eed48db3fc2f8e6a4128c95a7afe2d1cae
강한 <58686114+27kanghan@users.noreply.github.com>
2024-02-06 18:31:54 +0900

    Title: 연속되는 수 4, Time: 146ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240206/연속되는 수 4/continuous-number4.java
@@ -0,0 +1,43 @@
+import java.io.*;
+
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+
+    public static void main(String[] args) throws IOException {
+
+        int N = Integer.parseInt(br.readLine());
+
+        int arr[] = new int[N];
+
+        for(int i = 0; i<N; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int max = 0;
+        int plusCnt = 0;
+        for(int i = 0; i < N; i++){
+            if(i == 0 || arr[i] > arr[i-1] ){
+                plusCnt++;
+            }else {
+                plusCnt = 1;
+            }
+
+
+            max = Math.max(max, plusCnt);
+        }
+
+
+        sb.append(max);
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+
+}
