e6d1a064997327fc1f4424c791e67c89fbcb844f
Han Kang <58686114+27kanghan@users.noreply.github.com>
2024-02-09 18:44:01 +0900

    Title: 되돌아오기, Time: 95ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240209/되돌아오기/come-back.java
@@ -0,0 +1,75 @@
+import java.io.*;
+
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    static int dr[] = {1, 0, -1, 0};
+    static int dc[] = {0, 1, 0, -1};
+
+    public static void main(String[] args) throws IOException {
+
+        int N = Integer.parseInt(br.readLine());
+
+        int r = 0;
+        int c = 0;
+        int cnt = 0;
+        boolean flags = false;
+        int sum = 0;
+
+        for(int i = 0; i < N; i++){
+            st = new StringTokenizer(br.readLine());
+            char dir = st.nextToken().charAt(0);
+            int num = Integer.parseInt(st.nextToken());
+            int d = 0;
+            sum += num;
+
+            if(dir == 'N'){
+                d = 0;
+            }else if(dir == 'S'){
+                d = 2;
+
+            }else if(dir == 'W'){
+                d = 3;
+
+            }else if(dir == 'E'){
+                d = 1;
+            }
+            for(int j = 0; j < num; j++){
+                r = r+dr[d];
+                c = c+dc[d];
+                cnt++;
+
+                if(r == 0 && c == 0){
+                    flags = true;
+                    break;
+                }
+            }
+            if(flags){
+                break;
+            }
+
+        }
+        if(cnt > N*sum){
+            sb.append(-1);
+        }else {
+            sb.append(cnt);
+        }
+
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+
+    private static boolean inRange(int r, int c, int N, int M) {
+        return r >= 0 && r < N && c >= 0 && c < M;
+    }
+
+
+}
