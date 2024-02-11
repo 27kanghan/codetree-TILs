e88eaad0d156177456975c9e11587329ad22868a
Han Kang <58686114+27kanghan@users.noreply.github.com>
2024-02-11 19:50:21 +0900

    Title: 일렬로 서있는 소 2, Time: 102ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240211/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.java
@@ -0,0 +1,45 @@
+import java.io.*;
+
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+
+        int N = Integer.parseInt(br.readLine());
+
+        int arr[] = new int[N];
+
+        int cnt = 0;
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < N-2; i++) {
+            for (int j = i + 1; j < N-1; j++) {
+                for (int k = j + 1; k < N; k++) {
+                    int a = arr[i];
+                    int b = arr[j];
+                    int c = arr[k];
+
+                    if (i < j && j < k && a < b && b < c) {
+                        cnt++;
+                    }
+                }
+            }
+
+        }
+        sb.append(cnt);
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
