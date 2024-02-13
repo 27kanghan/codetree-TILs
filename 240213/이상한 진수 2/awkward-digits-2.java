4e1dc7bd2772db82f8eceb8081fb54951075d0b5
Han Kang <58686114+27kanghan@users.noreply.github.com>
2024-02-13 18:58:54 +0900

    Title: 이상한 진수 2, Time: 90ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240213/이상한 진수 2/awkward-digits-2.java
@@ -0,0 +1,69 @@
+import java.io.*;
+
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+
+        String str = br.readLine();
+
+        boolean flags = false;
+        int num = 0;
+        int max = Integer.MIN_VALUE;
+
+        char arr[] = new char [str.length()];
+
+        for(int i = 0; i < str.length(); i++){
+            if(str.charAt(i)-'0' == 0){
+                flags = true;
+            }
+        }
+        if(!flags) {
+            num = change(str);
+            sb.append(num);
+        }else{
+            for(int i = 0; i < str.length(); i++){
+                arr[i] = str.charAt(i);
+            }
+
+            for(int i = 1; i < str.length(); i++){
+                if(arr[i] == '0'){
+                    arr[i] = '1';
+                }
+
+                String newStr = String.valueOf(arr);
+
+                int sum = change(newStr);
+
+                max = Math.max(max, sum);
+
+                arr[i] = '0';
+            }
+            sb.append(max);
+        }
+
+
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+    private static int change(String str) {
+
+        int sum = 0;
+        int cnt = 0;
+
+        for(int i = str.length()-1; i >= 0; i--){
+            sum += (Math.pow(2,cnt)) * (str.charAt(i) - '0');
+            cnt++;
+        }
+
+        return sum;
+    }
+}
