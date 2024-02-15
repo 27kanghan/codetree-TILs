39854482f431179b455f471a36d7a508051ab671
Han Kang <58686114+27kanghan@users.noreply.github.com>
2024-02-15 22:20:40 +0900

    Title: G or H 3, Time: 88ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240215/G or H 3/G-or-H-3.java
@@ -0,0 +1,56 @@
+import java.io.*;
+
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int arr[] = new int[10001];
+
+        int maxIdx = Integer.MIN_VALUE;
+
+        for(int i = 0; i < N; i++){
+            st = new StringTokenizer(br.readLine());
+            int num = Integer.parseInt(st.nextToken());
+            char photo = st.nextToken().charAt(0);
+
+            maxIdx = Math.max(num, maxIdx);
+
+            if(photo == 'G'){
+                arr[num-1] = 1;
+            }else{
+                arr[num-1] = 2;
+            }
+        }
+
+
+        int max = Integer.MIN_VALUE;
+
+        for(int i = 0; i <= maxIdx - K; i++){
+            int sum = 0;
+            for(int j = i; j <= i+K; j++){
+                sum += arr[j];
+            }
+
+            max = Math.max(sum, max);
+        }
+
+
+        sb.append(max);
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+
+
+}
