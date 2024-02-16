1628f3eafd685cd26a5532e423b9672276aad9e9
Han Kang <58686114+27kanghan@users.noreply.github.com>
2024-02-16 19:47:00 +0900

    Title: G or H 2, Time: 107ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240216/G or H 2/G-or-H-2.java
@@ -0,0 +1,79 @@
+import java.io.*;
+
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+
+        st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+
+        int arr[] = new int[101];
+
+        int maxIdx = 0;
+        for(int i = 0; i < N; i++){
+            st = new StringTokenizer(br.readLine());
+            int idx = Integer.parseInt(st.nextToken());
+            char sign = st.nextToken().charAt(0);
+
+            maxIdx = Math.max(maxIdx, idx);
+
+            if(sign == 'G'){
+                arr[idx] = 1;
+            }else{
+                arr[idx] = 2;
+            }
+        }
+
+//        System.out.println(Arrays.toString(arr));
+        // G = 1; H = 2;
+        int max = Integer.MIN_VALUE;
+
+        for(int i = 0; i <= maxIdx; i++){
+
+            for(int j = 0; j <= maxIdx; j++){
+                int g = 0;
+                int h = 0;
+                int sum = 0;
+
+//                System.out.println("Arrays" + i );
+                for(int k = i; k <= maxIdx; k++){
+//                    System.out.print(arr[k] + " ");
+                    if(arr[k] == 1){
+                        g++;
+                    }else if(arr[k] == 2){
+                        h++;
+                    }
+                }
+
+//                System.out.println("valid");
+
+                if(g == h || h == 0 || g == 0){
+//                    System.out.println(true);
+                    max = Math.max(max, j - i -1 );
+                }
+
+            }
+
+        }
+        if(N == 1){
+            sb.append(0);
+        }else {
+            sb.append(max);
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+
+    }
+
+
+
+}
