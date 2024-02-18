a8c9178800a02466326d96273dc535535e2becba
Han Kang <58686114+27kanghan@users.noreply.github.com>
2024-02-18 22:50:50 +0900

    Title: 숫자 카운트, Time: 102ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240218/숫자 카운트/numeric-count.java
@@ -0,0 +1,108 @@
+import java.io.*;
+
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    static class Point {
+        int num;
+        int cnt1;
+        int cnt2;
+
+        public Point(int num, int cnt1, int cnt2) {
+            this.num = num;
+            this.cnt1 = cnt1;
+            this.cnt2 = cnt2;
+        }
+
+        @Override
+        public String toString() {
+            return "Point{" +
+                    "num=" + num +
+                    ", cnt1=" + cnt1 +
+                    ", cnt2=" + cnt2 +
+                    '}';
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        int N = Integer.parseInt(br.readLine());
+
+        Point arr[] = new Point[N];
+
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int num = Integer.parseInt(st.nextToken());
+            int cnt1 = Integer.parseInt(st.nextToken());
+            int cnt2 = Integer.parseInt(st.nextToken());
+
+            arr[i] = new Point(num, cnt1, cnt2);
+        }
+
+        int cnt = 0;
+
+
+        //숫자가 동일한 자리이다 -> 카운트 1, 숫자만 같다 => 카운트 2
+
+//        valid(arr, 324, N);
+
+        for (int i = 100; i < 1000; i += 100) {
+            for (int j = 10; j < 100; j += 10) {
+                for (int k = 1; k < 10; k++) {
+                    int res = i + j + k;
+                    if (valid(arr, res, N)) {
+                        cnt++;
+                    }
+                }
+            }
+        }
+        sb.append(cnt);
+
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+
+    }
+
+    private static boolean valid(Point[] arr, int res, int N) {
+
+
+        for (int i = 0; i < N; i++){
+            int cnt1 = 0;
+            int cnt2 = 0;
+            String str = String.valueOf(arr[i].num);
+            String str2 = String.valueOf(res);
+            if(str.substring(0,1).equals(str2.substring(0,1))) cnt1++;
+            if(str.substring(1,2).equals(str2.substring(1,2))) cnt1++;
+            if(str.substring(2,3).equals(str2.substring(2,3))) cnt1++;
+
+            if(cnt1 != arr[i].cnt1){
+                return false;
+            }
+
+
+            if(str.contains(str2.substring(0,1)) && !str.substring(0,1).equals(str2.substring(0,1))
+            || str.contains(str2.substring(1,2)) && !str.substring(1,2).equals(str2.substring(1,2))
+            || str.contains(str2.substring(2,3)) && !str.substring(2,3).equals(str2.substring(2,3))){
+
+                cnt2++;
+            }
+
+
+            if(arr[i].cnt2 != cnt2){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
+}
