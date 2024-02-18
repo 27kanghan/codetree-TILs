7e8cdd145a7631cba3d3db63f7f45398f50040dc
Han Kang <58686114+27kanghan@users.noreply.github.com>
2024-02-18 23:11:34 +0900

    Title: 숫자 카운트, Time: 114ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 240218/숫자 카운트/numeric-count.java
+++ 240218/숫자 카운트/numeric-count.java
@@ -47,18 +47,28 @@ public class Main {
 
         int cnt = 0;
 
+//        System.out.println(Arrays.toString(arr));
 
         //숫자가 동일한 자리이다 -> 카운트 1, 숫자만 같다 => 카운트 2
 
-//        valid(arr, 324, N);
+//        valid(arr, 174, N);
 
         for (int i = 100; i < 1000; i += 100) {
             for (int j = 10; j < 100; j += 10) {
                 for (int k = 1; k < 10; k++) {
-                    int res = i + j + k;
-                    if (valid(arr, res, N)) {
-                        cnt++;
-                    }
+
+                        int res = i + j + k;
+
+                        String str = String.valueOf(res);
+
+                        if(str.charAt(0) != str.charAt(1) && str.charAt(1) != str.charAt(2) && str.charAt(0) != str.charAt(2)) {
+
+                            if (valid(arr, res, N)) {
+//                                System.out.println("Success" + res);
+                                cnt++;
+                            }
+                        }
+
                 }
             }
         }
@@ -74,32 +84,42 @@ public class Main {
     private static boolean valid(Point[] arr, int res, int N) {
 
 
-        for (int i = 0; i < N; i++){
+        for (int i = 0; i < N; i++) {
             int cnt1 = 0;
             int cnt2 = 0;
             String str = String.valueOf(arr[i].num);
             String str2 = String.valueOf(res);
-            if(str.substring(0,1).equals(str2.substring(0,1))) cnt1++;
-            if(str.substring(1,2).equals(str2.substring(1,2))) cnt1++;
-            if(str.substring(2,3).equals(str2.substring(2,3))) cnt1++;
+            if (str.substring(0, 1).equals(str2.substring(0, 1))) cnt1++;
+            if (str.substring(1, 2).equals(str2.substring(1, 2))) cnt1++;
+            if (str.substring(2, 3).equals(str2.substring(2, 3))) cnt1++;
 
-            if(cnt1 != arr[i].cnt1){
+//            System.out.println("arr" + arr[i].num + " " + res + " 1pass cnt " + cnt1 + " arr cnt " + arr[i].cnt1);
+
+            if (cnt1 != arr[i].cnt1) {
                 return false;
             }
 
+//            System.out.println(str.substring(0,1).equals(str2.substring(0,1)));
+//            System.out.println(str.contains(str2.substring(0,1)) && !str.substring(0,1).equals(str2.substring(0,1)));
+//            System.out.println(str.contains(str2.substring(1,2)) && !str.substring(1,2).equals(str2.substring(1,2)));
+//            System.out.println(str.contains(str2.substring(2,3)) && !str.substring(2,3).equals(str2.substring(2,3)));
 
-            if(str.contains(str2.substring(0,1)) && !str.substring(0,1).equals(str2.substring(0,1))
-            || str.contains(str2.substring(1,2)) && !str.substring(1,2).equals(str2.substring(1,2))
-            || str.contains(str2.substring(2,3)) && !str.substring(2,3).equals(str2.substring(2,3))){
+            if ((str.contains(str2.substring(0, 1)) && !str.substring(0, 1).equals(str2.substring(0, 1)))
+                    || (str.contains(str2.substring(1, 2)) && !str.substring(1, 2).equals(str2.substring(1, 2)))
+                    || (str.contains(str2.substring(2, 3)) && !str.substring(2, 3).equals(str2.substring(2, 3)))) {
 
                 cnt2++;
             }
 
+//            System.out.println("arr" + arr[i].num + " " + res + " 2pass cnt2 " + cnt2 + " arr cnt2 " + arr[i].cnt2);
 
-            if(arr[i].cnt2 != cnt2){
+
+            if (arr[i].cnt2 != cnt2) {
+//                System.out.println("false " + res);
                 return false;
             }
         }
+//        System.out.println("res" + res);
 
         return true;
     }
