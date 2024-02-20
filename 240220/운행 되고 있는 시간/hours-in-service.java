414537351730017c862566f0ac3cee909b0d3e12
Han Kang <58686114+27kanghan@users.noreply.github.com>
2024-02-20 20:41:16 +0900

    Title: 운행 되고 있는 시간, Time: 93ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240220/운행 되고 있는 시간/hours-in-service.java
@@ -0,0 +1,83 @@
+import java.io.*;
+
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    static class Point {
+        int x;
+        int y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public String toString() {
+            return "Point{" +
+                    "x=" + x +
+                    ", y=" + y +
+                    '}';
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        int N = Integer.parseInt(br.readLine());
+
+        Point arr[] = new Point[N];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            arr[i] = new Point(x, y);
+        }
+
+        int max = 0;
+
+        for(int i = 0; i < N; i++){
+
+            for(int j = i+1; j< N; j++){
+                int time[] = new int[1001];
+                int cnt = 0;
+                int a1 = arr[i].x;
+                int b1 = arr[i].y;
+
+                int a2 = arr[j].x;
+                int b2 = arr[j].y;
+
+                for(int k = a1; k < b1; k++){
+                    time[k] = 1;
+                }
+                for(int k = a2; k < b2; k++){
+                    time[k] = 1;
+                }
+
+                for(int k = 0; k < 1001; k++){
+                    if(time[k] == 1){
+                        cnt++;
+                    }
+                }
+
+                max = Math.max(cnt, max);
+
+            }
+        }
+
+        sb.append(max);
+
+
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+
+    }
+}
