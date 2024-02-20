0c9da7c7fd9fa9c7e5fb433b6a9e8050431f053b
Han Kang <58686114+27kanghan@users.noreply.github.com>
2024-02-20 20:31:01 +0900

    Title: 삼각형 만들기, Time: 113ms, Memory: 9MB, Status: Passed - CodeTree

--- 240220/삼각형 만들기/create-triangle.java
+++ 240220/삼각형 만들기/create-triangle.java
@@ -8,7 +8,7 @@ public class Main {
     static StringTokenizer st;
     static StringBuilder sb = new StringBuilder();
 
-    static class Point{
+    static class Point {
         int x;
         int y;
 
@@ -32,20 +32,20 @@ public class Main {
 
         Point arr[] = new Point[N];
 
-        for(int i = 0; i < N; i++){
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
 
-            arr[i] = new Point(x,y);
+            arr[i] = new Point(x, y);
         }
 
         int max = 0;
 
-        for(int i = 0; i < N; i++){
+        for (int i = 0; i < N; i++) {
 
-            for(int j = i+1; j <N; j++){
-                for(int k = j+1; k < N; k++) {
+            for (int j = i + 1; j < N; j++) {
+                for (int k = j + 1; k < N; k++) {
                     int x1 = arr[i].x;
                     int y1 = arr[i].y;
                     int x2 = arr[j].x;
@@ -53,8 +53,15 @@ public class Main {
                     int x3 = arr[k].x;
                     int y3 = arr[k].y;
 
-                    int area = Math.abs((x1*y2 + x2*y3 + x3*y1) - (x2*y1 + x3*y2 + x1*y3));
+                    int area = 0;
 
+                    if (x1 == x2 || x2 == x3 || x3 == x1) {
+                        if (y1 == y2 || y1 == y3 || y2 == y3) {
+
+
+                            area = Math.abs((x1 * y2 + x2 * y3 + x3 * y1) - (x2 * y1 + x3 * y2 + x1 * y3));
+                        }
+                    }
                     max = Math.max(area, max);
                 }
             }
