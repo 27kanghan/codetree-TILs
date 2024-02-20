a0819bdb325e77021189622d9806360a102fb457
Han Kang <58686114+27kanghan@users.noreply.github.com>
2024-02-20 23:12:26 +0900

    Title: 겹치지 않는 선분 2, Time: 87ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240220/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.java
+++ 240220/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.java
@@ -8,14 +8,14 @@ public class Main {
     static StringTokenizer st;
     static StringBuilder sb = new StringBuilder();
 
-    static class Point implements Comparable<Point>{
+    static class Point implements Comparable<Point> {
         int x;
         int y;
         boolean flags;
 
-        Point(int x, int y, boolean flags){
-            this. x = x;
-            this. y = y;
+        Point(int x, int y, boolean flags) {
+            this.x = x;
+            this.y = y;
             this.flags = flags;
         }
 
@@ -35,70 +35,68 @@ public class Main {
     }
 
 
-
     public static void main(String[] args) throws IOException {
 
         int N = Integer.parseInt(br.readLine());
 
         Point arr[] = new Point[N];
 
-        for(int i = 0; i < N; i++){
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
 
-            arr[i] = new Point(x,y, false);
+            arr[i] = new Point(x, y, false);
 
         }
 
-//        Arrays.sort(arr);
+        Arrays.sort(arr);
 
         int cnt = 0;
 
-        for(int i = 0; i < N; i++){
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (i == j) continue;
+                int x = arr[i].x;
+                int y = arr[i].y;
 
-            for(int j = i+1; j < N; j++){
-//                if(i==j) continue;
-                    int x = arr[i].x;
-                    int y = arr[i].y;
+                int a = arr[j].x;
+                int b = arr[j].y;
 
-                    int a = arr[j].x;
-                    int b = arr[j].y;
-
-                    // a랑 b가 x,y안에 있거나 x랑 y가 a,b에 있다.
+                // a랑 b가 x,y안에 있거나 x랑 y가 a,b에 있다.
 
 
-                    if (x < y) {
-                        if(a < b){
-                            if(a > x && a < y && b < y && b > x){
-                                arr[i].flags =true;
-                                arr[j].flags =true;
-                            }
-                        }else if(a > b ){
-                            if(a < y){
-                                arr[i].flags =true;
-                                arr[j].flags =true;
-                            }
+                if (x < y) {
+                    if (a < b) {
+                        if (a > x && a < y && b < y && b > x) {
+                            arr[i].flags = true;
+                            arr[j].flags = true;
                         }
-                    }else if(x > y){
-                        if(a > b){
-                            if(a < y){
-                                arr[i].flags =true;
-                                arr[j].flags =true;
-                            }
-                        }else if(a < b){
-                            if(a < y){
-                                arr[i].flags =true;
-                                arr[j].flags =true;
-                            }
+                    } else if (a > b) {
+                        if (a < y) {
+                            arr[i].flags = true;
+                            arr[j].flags = true;
                         }
                     }
+                } else if (x > y) {
+                    if (a > b) {
+                        if (a < y) {
+                            arr[i].flags = true;
+                            arr[j].flags = true;
+                        }
+                    } else if (a < b) {
+                        if (a < y) {
+                            arr[i].flags = true;
+                            arr[j].flags = true;
+                        }
+                    }
+                }
 
             }
         }
 
-        for(int i = 0; i < N; i++){
-            if(!arr[i].flags){
+        for (int i = 0; i < N; i++) {
+            if (!arr[i].flags) {
                 cnt++;
             }
         }
