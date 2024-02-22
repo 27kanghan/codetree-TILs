f8a8b324bbade0f626449a9ac816cad8e0a27a63
Han Kang <58686114+27kanghan@users.noreply.github.com>
2024-02-22 22:03:04 +0900

    Title: 달에서 무게 구하기, Time: 89ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240222/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 13;
+        double b = 0.165000;
+        double res = a * b;
+
+        System.out.print(a + " * 0.165000" +  " = " );
+        System.out.printf("%.6f", res);
+    }
+}
