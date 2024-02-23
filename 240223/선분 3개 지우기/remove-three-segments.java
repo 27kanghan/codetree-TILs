aafd5ff4fafb55e54917ef8ec4163135494327fb
Han Kang <58686114+27kanghan@users.noreply.github.com>
2024-02-23 18:56:08 +0900

    Title: 선분 3개 지우기, Time: 94ms, Memory: 8MB, Status: Passed - CodeTree

--- 240223/선분 3개 지우기/remove-three-segments.java
+++ 240223/선분 3개 지우기/remove-three-segments.java
@@ -33,70 +33,47 @@ public class Main {
             arr[i] = new Point(x, y);
         }
 
-        int line[] = new int[101];
 
 
 
-//        System.out.println(Arrays.toString(line));
-
-        for(int i = 0; i < N; i++){
-            int x = arr[i].x;
-            int y = arr[i].y;
-
-            for(int j = x; j <= y; j++){
-                line[j]++;
-            }
-        }
 
         int cnt = 0;
 
         for(int i = 0; i < N; i++){
-            int xi = arr[i].x;
-            int yi = arr[i].y;
-
-            for(int n = xi; n <= yi; n++){
-                line[n]--;
-            }
 
             for(int j = i+1; j < N; j++){
-                int xj = arr[j].x;
-                int yj = arr[j].y;
-                for(int n = xj; n <= yj; n++){
-                    line[n]--;
-                }
                 for(int k = j+1; k < N; k++){
-                    int xk = arr[k].x;
-                    int yk = arr[k].y;
 
-                    for(int n = xk; n <= yk; n++){
-                        line[n]--;
+                    int line[] = new int[101];
+
+
+                    for(int x = 0; x < N; x++) {
+                        if (x == i || x == j || x == k) continue;
+
+                        int xk = arr[x].x;
+                        int yk = arr[x].y;
+
+
+                        for (int n = xk; n <= yk; n++) {
+                            line[n]++;
+                        }
                     }
 
                     boolean flags = true;
-
-//                    System.out.println(Arrays.toString(line));
 
                     for(int n = 0; n < 101; n++){
                         if(line[n] > 1){
                             flags = false;
                             break;
                         }
-                    }
-//                    System.out.println(flags);
-                    if(flags){
+
+                    }if(flags){
                         cnt++;
                     }
 
-                    for(int n = xk; n <= yk; n++){
-                        line[n]++;
-                    }
+
                 }
-                for(int n = xj; n <= yj; n++){
-                    line[n]++;
-                }
-            }
-            for(int n = xi; n <= yi; n++){
-                line[n]++;
+
             }
         }
 
