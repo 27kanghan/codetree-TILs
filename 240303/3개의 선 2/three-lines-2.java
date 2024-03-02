3b71126fc8fd9dd6ce547dab4e73a78d48ae3323
Han Kang <58686114+27kanghan@users.noreply.github.com>
2024-03-03 02:42:07 +0900

    Title: 3개의 선 2, Time: 86ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240303/3개의 선 2/three-lines-2.java
+++ 240303/3개의 선 2/three-lines-2.java
@@ -8,11 +8,11 @@ public class Main {
     static StringTokenizer st;
     static StringBuilder sb = new StringBuilder();
 
-    static class Point{
+    static class Point {
         int x;
         int y;
 
-        private Point(int x, int y){
+        private Point(int x, int y) {
             this.x = x;
             this.y = y;
         }
@@ -23,17 +23,16 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
 
 
-
         Point arr[] = new Point[N];
 
         int max = Integer.MIN_VALUE;
 
 
-        for(int i = 0; i < N; i++){
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
-            arr[i] = new Point(x,y);
+            arr[i] = new Point(x, y);
 
             max = Math.max(max, Math.max(y, x));
 
@@ -43,61 +42,87 @@ public class Main {
 
         boolean res = false;
 
+        String dir[] = {"xxx", "xxy", "xyx", "yxx", "xyy", "yxy", "yyx", "yyy"};
+        boolean flags2 = false;
+        for (int i = 0; i <= 10; i++) {
+            for (int j = 0; j <= 10; j++) {
+                for (int k = 0; k <= 10; k++) {
 
-        for(int i = 0; i <= 10; i++){
-            for(int j = 0; j <= 10; j++){
-                for(int k = 0; k <= 10; k++){
 
-                    boolean flags = false;
-
-                    for(int n = 0; n < N; n++){
-                        // System.out.println(n);
-                        int x = arr[n].x;
-                        int y = arr[n].y;
-//                        System.out.println(x + " : " + y);
-//                        System.out.println(i + " : " + j + " :  " + k);
-
-                        // x 3
-                        if(i == x || j == x || k == x){
-//                            System.out.println("1번");
-                            flags = true;
+                    for (int d = 0; d < 8; d++) {
+                        boolean flags = false;
+                        for (int n = 0; n < N; n++) {
+                            int x = arr[n].x;
+                            int y = arr[n].y;
+                            if (d == 0) {
+                                if (i == x || j == x || k == x) flags = true;
+                                else {
+                                    flags = false;
+                                    break;
+                                }
+                            } else if (d == 1) {
+                                if (i == x || j == x || k == y) flags = true;
+                                else {
+                                    flags = false;
+                                    break;
+                                }
+                            } else if (d == 2) {
+                                if (i == x || j == y || k == x) flags = true;
+                                else {
+                                    flags = false;
+                                    break;
+                                }
+                            } else if (d == 3) {
+                                if (i == y || j == x || k == x) flags = true;
+                                else {
+                                    flags = false;
+                                    break;
+                                }
+                            } else if (d == 4) {
+                                if (i == y || j == y || k == x) flags = true;
+                                else {
+                                    flags = false;
+                                    break;
+                                }
+                            } else if (d == 5) {
+                                if (i == y || j == x || k == y) flags = true;
+                                else {
+                                    flags = false;
+                                    break;
+                                }
+                            } else if (d == 6) {
+                                if (i == x || j == y || k == y) flags = true;
+                                else {
+                                    flags = false;
+                                    break;
+                                }
+                            } else {
+                                if (i == y || j == y || y == x) flags = true;
+                                else {
+                                    flags = false;
+                                    break;
+                                }
+                            }
                         }
-                        // x2 y1
-                        else if((i == x || j == x || k == y) || (i == x || j == y || k == x) ||
-                                (i == y || j == x || k == x)){
-//                            System.out.println("2번");
-                            flags = true;
-                        }
-                        // x1 y2
-
-                        else if((i == x || j == y || k == y) || (i == y || j == y || k == x) ||
-                                (i == y || j == x || k == y)){
-//                            System.out.println("3번");
-                            flags = true;
-                        }
-                        // y 3
-                        else if(i == y || j == y || k == y){
-//                            System.out.println("4번");
-                            flags = true;
-                        }else{
-//                            System.out.println("5번");
-                            flags = false;
+                        if (flags) {
+                            flags2 = true;
+                            val = 1;
                             break;
                         }
-
                     }
-//                    System.out.println(flags);
-
-                    if(flags){
-//                        System.out.println("통과");
-                        res = true;
-                        val = 1;
+                    if (flags2) {
                         break;
                     }
+
+
                 }
-                if(res) break;
+                if (flags2) {
+                    break;
+                }
             }
-            if(res) break;
+            if (flags2) {
+                break;
+            }
         }
 
 
@@ -108,4 +133,6 @@ public class Main {
         bw.close();
 
     }
+
+
 }
