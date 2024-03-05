d6e6519685bf1bcea6334e8197eb60a16e992959
Han Kang <58686114+27kanghan@users.noreply.github.com>
2024-03-05 21:17:22 +0900

    Title: 독서실의 거리두기 5, Time: 105ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240305/독서실의 거리두기 5/study-cafe-keeping-distance-5.java
+++ 240305/독서실의 거리두기 5/study-cafe-keeping-distance-5.java
@@ -22,6 +22,7 @@ public class Main {
         }
 
         int max = Integer.MIN_VALUE;
+
         boolean flags = false;
         for (int i = 1; i < N - 1; i++) {
             if (arr[i] == 1 && arr[i - 1] == 1 && arr[i + 1] == 1) {
@@ -54,40 +55,54 @@ public class Main {
 //                    continue;
 //                }
 
-//                System.out.println(Arrays.toString(copy));
-
-                int diff = 0;
-
                 int minDiff = Integer.MAX_VALUE;
-                for (int j = 0; j < N-1; j++) {
 
-                    if (copy[j + 1] == 0) {
-                        diff++;
+                for(int j = 0; j < N-1; j++){
+
+                    if(copy[j] != 1) continue;
+                    boolean flag = false;
+                    int diff = 0;
+                    int min = Integer.MAX_VALUE;
+
+                    for(int k = j; k < N-1; k++){
+                        if(copy[k+1] == 0){
+                            diff++;
+                        }else if(copy[k+1] == 1){
+                            diff++;
+//                            flag = true;
+                            minDiff = Math.min(diff,minDiff);
+//                            System.out.println("mindiff" + j + " " + minDiff);
+//                            System.out.println("diff" + j + " " + diff);
+                            break;
+                        }
                     }
-                    if(copy[j+1] == 1){
-                        diff++;
-                        minDiff = Math.min(minDiff, diff);
-                        diff = 0;
+
+
+                    if(flag){
+
                     }
+
+                    diff = 0;
+
 
                 }
 
-
                 max = Math.max(minDiff, max);
 
+//                System.out.println(Arrays.toString(copy));
             }
+
+            if (flags) {
+                sb.append(1);
+            } else {
+                sb.append(max);
+            }
+            bw.write(sb.toString());
+            bw.flush();
+            bw.close();
+
         }
 
-        if (flags) {
-            sb.append(1);
-        } else {
-            sb.append(max);
-        }
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
 
     }
-
-
 }
