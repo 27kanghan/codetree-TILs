886a5fc5b3706347f573246636dc2d96bc7f6a7c
Han Kang <58686114+27kanghan@users.noreply.github.com>
2024-03-19 19:41:15 +0900

    Title: 언덕 깎기, Time: 205ms, Memory: 13MB, Status: Passed - CodeTree

--- 240319/언덕 깎기/hill-cutting.java
+++ 240319/언덕 깎기/hill-cutting.java
@@ -1,68 +1,41 @@
-import java.io.*;
-
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static StringTokenizer st;
-    static StringBuilder sb = new StringBuilder();
+    public static final int INT_MAX = Integer.MAX_VALUE;
+    public static final int MAX_H = 100;
+    public static final int MAX_N = 1000;
+    
+    public static int n;
+    public static int k = 17;
+    public static int[] arr = new int[MAX_N];
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 입력:
+        n = sc.nextInt();
+        for(int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
 
-        int N = Integer.parseInt(br.readLine());
+        int ans = INT_MAX;
+        // 크기가 K인 모든 구간을 잡아
+        // 해당 구간 안에 들어오게 언덕을 깎고
+        // 그 비용 중 중 최솟값을 계산합니다.
+        for(int i = 0; i <= MAX_H; i++) {
+            // 구간 [i, i + k] 에서의 언덕을 깎는
+            // 비용을 계산합니다.
+            // i + k보다 높은 언덕은 높이가 i + k가 되게 깎으며
+            // i보다 낮은 언덕은 높이가 i가 되게 쌓으면 됩니다.
+            int cost = 0;
+            for(int j = 0; j < n; j++) {
+                if(arr[j] < i)
+                    cost += (arr[j] - i) * (arr[j] - i);
+                if(arr[j] > i + k)
+                    cost += (arr[j] - i - k) * (arr[j] - i - k);
+            }
 
-        int height[] = new int[N];
-        int count[] = new int[N];
-
-        for(int i = 0; i< N; i++){
-            height[i] = Integer.parseInt(br.readLine());
+            ans = Math.min(ans, cost);
         }
-
-
-        int res = 0;
-        int totalCnt = 0;
-        int minIdx = 0;
-        int maxIdx = 0;
-        while(true){
-            int min = Integer.MAX_VALUE;
-            int max = Integer.MIN_VALUE;
-            for(int i = 0; i < N; i++){
-                if(min >= height[i]) {
-                    minIdx = i;
-                    min = height[i];
-                }
-                if(max <= height[i]){
-                    maxIdx = i;
-                    max = height[i];
-                }
-            }
-            if(max - min <= 17){
-                for(int i = 0; i < N; i++){
-                    res += count[i] * count[i];
-                }
-                break;
-            }
-
-            if(totalCnt % 2 == 0){
-                height[minIdx]++;
-                count[minIdx]++;
-            }else{
-                height[maxIdx]--;
-                count[maxIdx]++;
-            }
-            totalCnt++;
-            
-
-        }
-
-        sb.append(res);
-
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-
-
-
+        
+        System.out.print(ans);
     }
 }
