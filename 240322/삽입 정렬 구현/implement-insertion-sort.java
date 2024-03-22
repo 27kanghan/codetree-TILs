31f43e195fc89a5e4b6ccea22524ee1209b39a39
Han Kang <58686114+27kanghan@users.noreply.github.com>
2024-03-22 21:50:52 +0900

    Title: 삽입 정렬 구현, Time: 87ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240322/삽입 정렬 구현/implement-insertion-sort.java
@@ -0,0 +1,41 @@
+import java.io.*;
+
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+
+    public static void main(String[] args) throws IOException {
+
+        int N = Integer.parseInt(br.readLine());
+
+        int arr[] = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i = 0; i < N; i++){
+            int j = i-1;
+            int key = arr[i];
+            while(j >= 0 && arr[j] > key){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = key;
+        }
+
+        for(int i = 0; i < N; i++){
+            sb.append(arr[i]);
+            sb.append(" ");
+        }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
