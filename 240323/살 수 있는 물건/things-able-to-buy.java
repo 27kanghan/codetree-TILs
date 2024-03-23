0e6e4a46347be56b00bd357e0991f163c7e64441
Han Kang <58686114+27kanghan@users.noreply.github.com>
2024-03-23 20:53:32 +0900

    Title: 살 수 있는 물건, Time: 95ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240323/살 수 있는 물건/things-able-to-buy.java
@@ -0,0 +1,29 @@
+import java.io.*;
+
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+
+    public static void main(String[] args) throws IOException {
+
+       
+        int N = Integer.parseInt(br.readLine());
+        
+        if(N >= 3000){
+            sb.append("book");
+        }else if(N >= 1000){
+            sb.append("mask");
+        }else{
+            sb.append("no");
+        }
+        
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
