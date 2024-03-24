13df1db466ca5159477792514b6b5c43ba02cc0c
Han Kang <58686114+27kanghan@users.noreply.github.com>
2024-03-24 11:17:08 +0900

    Title: 특정 조건 두 정수 비교, Time: 109ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240324/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.java
@@ -0,0 +1,33 @@
+import java.io.*;
+
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+
+    public static void main(String[] args) throws IOException {
+
+
+        st = new StringTokenizer(br.readLine());
+       int a = Integer.parseInt(st.nextToken());
+       int b = Integer.parseInt(st.nextToken());
+
+        if( a < b){
+            sb.append(1);
+            sb.append(" ");
+            sb.append(0);
+        }else if( b < a ){
+            sb.append(0);
+            sb.append(" ");
+            sb.append(1);
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
