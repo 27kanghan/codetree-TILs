fc337b5637605a2ccab83f6481616c04cb349f22
Han Kang <58686114+27kanghan@users.noreply.github.com>
2024-03-30 20:20:19 +0900

    Title: 점수 비교, Time: 106ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240330/점수 비교/score-comparison.java
@@ -0,0 +1,35 @@
+import java.io.*;
+
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+
+    public static void main(String[] args) throws IOException {
+        
+        
+        st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+
+
+        if(a > b && c > d){
+            sb.append(1);
+        }else{
+            sb.append(0);
+        }
+
+
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
