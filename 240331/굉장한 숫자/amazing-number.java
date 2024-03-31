0b8486ff08acaf094c45602993c565b436c43841
Han Kang <58686114+27kanghan@users.noreply.github.com>
2024-03-31 23:35:33 +0900

    Title: 굉장한 숫자, Time: 97ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240331/굉장한 숫자/amazing-number.java
@@ -0,0 +1,31 @@
+import java.io.*;
+
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+
+    public static void main(String[] args) throws IOException {
+
+
+        int a = Integer.parseInt(br.readLine());
+
+
+        if((a % 2 != 0 && a % 3 == 0) || (a % 2 == 0 && a % 5 == 0) )
+        {
+            sb.append("true");
+        }else{
+            sb.append("false");
+        }
+
+
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
