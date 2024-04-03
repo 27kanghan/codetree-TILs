4e7ce30718f3d39e0ae6e369ad7cd5a7f5302602
Han Kang <58686114+27kanghan@users.noreply.github.com>
2024-04-03 22:39:50 +0900

    Title: 장학금, Time: 81ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240403/장학금/scholarship.java
@@ -0,0 +1,36 @@
+import java.io.*;
+
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+
+    public static void main(String[] args) throws IOException {
+
+
+        st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+
+        if(a >= 90){
+            if(b >= 95){
+                sb.append(100000);
+            }else if(b >= 90){
+                sb.append(50000);
+            }
+        }else{
+            sb.append(0);
+        }
+
+
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
