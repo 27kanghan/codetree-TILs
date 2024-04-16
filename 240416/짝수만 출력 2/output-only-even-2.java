25f2622a2357b3993b2bedde881e7ab1337422cf
Han Kang <58686114+27kanghan@users.noreply.github.com>
2024-04-16 20:18:50 +0900

    Title: 짝수만 출력 2, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240416/짝수만 출력 2/output-only-even-2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+//        int a = 26;
+        while(true){
+            if(a < b) break;
+
+            if(a%2==0)
+            System.out.print(a + " ");
+
+            a--;
+        }
+    }
+}
