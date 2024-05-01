f7d8057a5c660a18a97d0188f82b317410732ce8
Han Kang <58686114+27kanghan@users.noreply.github.com>
2024-05-01 21:36:42 +0900

    Title: a의 b승, Time: 192ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240501/a의 b승/a-to-the-power-of-b.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+//
+        int a= sc.nextInt();
+        int b = sc.nextInt();
+//
+//        int big = Math.max(a,b);
+//        int small = Math.min(a,b);
+
+        int multi = 1;
+
+
+        System.out.println((int)Math.pow(a,b));
+    }
+}
