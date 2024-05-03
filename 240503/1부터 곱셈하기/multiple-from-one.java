e9f8f64e30d60fca0a301a84eba3281008f72777
Han Kang <58686114+27kanghan@users.noreply.github.com>
2024-05-03 20:37:14 +0900

    Title: 1부터 곱셈하기, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240503/1부터 곱셈하기/multiple-from-one.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+//
+        int a= sc.nextInt();
+//        int b = sc.nextInt();
+//
+//        int big = Math.max(a,b);
+//        int small = Math.min(a,b);
+
+        int sum = 1;
+        int res = 0;
+
+        for(int i = 1; i <= 100; i++){
+            sum *= i;
+            if(sum >= a){
+                res = i;
+                System.out.println(res);
+                break;
+            }
+
+        }
+
+//        System.out.println(cnt);
+    }
+}
