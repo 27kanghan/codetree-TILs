f623b6a9b651867a614e172d057f5ce344fa2a5b
Han Kang <58686114+27kanghan@users.noreply.github.com>
2024-05-04 22:26:36 +0900

    Title: 20대의 평균나이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240504/20대의 평균나이/average-age-of-20.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+//
+
+        double sum = 0;
+
+        double cnt = 0;
+        double avg = 0;
+
+        while(true){
+
+            int a= sc.nextInt();
+
+            sum+=a;
+
+            cnt++;
+
+            if(a >= 30){
+                sum -= a;
+                cnt--;
+
+                avg = sum / cnt;
+
+                break;
+            }
+
+
+        }
+
+        System.out.printf("%.2f", avg);
+    }
+}
