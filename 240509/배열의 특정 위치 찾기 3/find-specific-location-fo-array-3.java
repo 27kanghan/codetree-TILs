08be264ca7e22601eda246bdb82446dbe136234c
Han Kang <58686114+27kanghan@users.noreply.github.com>
2024-05-09 20:53:01 +0900

    Title: 배열의 특정 위치 찾기 3, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240509/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+//
+
+        int arr[] = new int[10];
+
+        int sum = 0;
+        int idx = 0;
+
+        while(true){
+            int a = sc.nextInt();
+
+            if(a == 0){
+                break;
+            }
+
+
+            arr[idx] = a;
+
+            idx++;
+
+        }
+
+        for(int i = idx-3; i < idx; i++){
+            sum += arr[i];
+        }
+
+        System.out.println(sum);
+
+
+    }
+}
