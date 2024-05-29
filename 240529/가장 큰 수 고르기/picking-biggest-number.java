6e683b4f867894000c9ed5d576658a2e8726866d
Han Kang <58686114+27kanghan@users.noreply.github.com>
2024-05-29 22:14:48 +0900

    Title: 가장 큰 수 고르기, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240529/가장 큰 수 고르기/picking-biggest-number.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int max = -1;
+      for(int i = 0; i < 10; i++){
+
+          int a = sc.nextInt();
+          
+          max = Math.max(max,a);
+      }
+
+        System.out.println(max);
+
+    }
+}
