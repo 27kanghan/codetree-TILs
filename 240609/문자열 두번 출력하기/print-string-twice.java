da6db4821c254b89cb2ecfb54c6e2ae087fda711
Han Kang <58686114+27kanghan@users.noreply.github.com>
2024-06-09 20:54:14 +0900

    Title: 문자열 두번 출력하기, Time: 104ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240609/문자열 두번 출력하기/print-string-twice.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+       
+
+        String src = "LeeBrosCode";
+        
+        System.out.println(src);
+        System.out.println(src);
+
+    }
+}
