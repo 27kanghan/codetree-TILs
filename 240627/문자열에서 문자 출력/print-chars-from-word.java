79d40d92cb4596bda0079ada8cfbc234cc158d20
Han Kang <58686114+27kanghan@users.noreply.github.com>
2024-06-30 22:21:20 +0900

    Update print-chars-from-word.java

--- 240627/문자열에서 문자 출력/print-chars-from-word.java
+++ 240627/문자열에서 문자 출력/print-chars-from-word.java
@@ -8,6 +8,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         String str = sc.next();
+        //
 
         for(int i =0; i < str.length(); i++){
             System.out.println(str.charAt(i));
