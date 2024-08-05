0090e1ab8d2dc1a3d7f81df00c523aefc4ab0075
Han Kang <58686114+27kanghan@users.noreply.github.com>
2024-08-05 21:54:02 +0900

    Title: 일 수 구하기, Time: 143ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240805/일 수 구하기/number-of-days-in-month.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        int arr[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30 ,31};
+
+        Scanner sc = new Scanner(System.in);
+
+
+        int a = sc.nextInt();
+
+        System.out.println(arr[a-1]);
+
+
+    }
+}
