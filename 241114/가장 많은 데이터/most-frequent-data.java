6b07549d386fdcbb4a4e3c1dc553468ba7c2d9a4
Han Kang <58686114+27kanghan@users.noreply.github.com>
2024-11-14 22:50:55 +0900

    Title: 가장 많은 데이터, Time: 87ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 241114/가장 많은 데이터/most-frequent-data.java
@@ -0,0 +1,51 @@
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.Stack;
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        HashMap<String,Integer> hs = new HashMap<>();
+
+        int N = Integer.parseInt(br.readLine());
+
+        int max = Integer.MIN_VALUE;
+
+        for(int i = 0; i < N; i++){
+            String word = br.readLine();
+
+            if(!hs.containsKey(word)){
+                hs.put(word, 1);
+                continue;
+            }
+
+
+            hs.put(word, hs.get(word)+1);
+
+            max = Math.max(hs.get(word), max);
+        }
+
+        sb.append(max);
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+
+
+
+
+
+
+
+    }
+
+}
