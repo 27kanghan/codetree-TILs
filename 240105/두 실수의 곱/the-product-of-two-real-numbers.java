bbc54d917f2b60662e676d55f5842784ebd5a5ac
Han Kang <58686114+27kanghan@users.noreply.github.com>
2024-12-23 21:08:33 +0900

    Update the-product-of-two-real-numbers.java

--- 240105/두 실수의 곱/the-product-of-two-real-numbers.java
+++ 240105/두 실수의 곱/the-product-of-two-real-numbers.java
@@ -8,7 +8,6 @@ public class Main {
 //        StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
 
-
         double a = 5.26;
         double b = 8.27;
 
