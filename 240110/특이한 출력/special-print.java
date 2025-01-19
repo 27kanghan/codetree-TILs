7d7219e4fa97d4dbd3b1a36a3cd18aa0f8215974
Han Kang <58686114+27kanghan@users.noreply.github.com>
2025-01-19 23:18:18 +0900

    Update special-print.java

--- 240110/특이한 출력/special-print.java
+++ 240110/특이한 출력/special-print.java
@@ -8,6 +8,7 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
 
+        
         int n = Integer.parseInt(st.nextToken());
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= n; j ++){
