952df57f4d903503ac3ead5a8de9c14cf3771bb6
Han Kang <58686114+27kanghan@users.noreply.github.com>
2025-01-31 21:46:28 +0900

    Title: 두수의 곱과 몫, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 250131/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.py
+++ 250131/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.py
@@ -5,5 +5,5 @@ arr = a.split();
 num1 = int(arr[0]);
 num2 = int(arr[1]);
 
-print(f"300 * 5 = {num1*num2}");
-print(f"300 / 5 = {num1//num2}");
+print(f"{num1} * {num2} = {num1*num2}");
+print(f"{num1} / {num2} = {int(num1/num2)}");
