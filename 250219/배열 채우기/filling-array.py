d17205352252dfaa25a0a636b6fbb0ca23e5c65a
Han Kang <58686114+27kanghan@users.noreply.github.com>
2025-02-19 02:30:44 +0900

    Title: 배열 채우기, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250219/배열 채우기/filling-array.py
+++ 250219/배열 채우기/filling-array.py
@@ -1,6 +1,10 @@
 arr = list(map(int, input().split()));
 
+li = list();
 for i in range (len(arr)-1,-1 , -1) :
     if(arr[i] == 0 ) :
         continue;
-    print(arr[i], end=' ');
+    li.append(arr[i]);
+
+for i in range (len(li)) :
+    print(li[i], end= ' ');
