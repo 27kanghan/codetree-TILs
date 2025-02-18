58b972238b406955427fec734d46d8bf81d00dc7
Han Kang <58686114+27kanghan@users.noreply.github.com>
2025-02-19 03:27:39 +0900

    Title: 10개 입력받기, Time: 75ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 250219/10개 입력받기/receive-10-inputs.py
@@ -0,0 +1,18 @@
+arr = list(map(int, input().split()));
+
+cnt = 0;
+
+# 0 개수 찾기
+
+for en in arr :
+    if en == 0 :
+        break;
+    cnt += 1;
+
+sum_val = 0;
+
+for i in range (0, cnt+1) :
+    sum_val += arr[i];
+
+
+print(f"{sum_val} {sum_val/cnt:.1f}");
