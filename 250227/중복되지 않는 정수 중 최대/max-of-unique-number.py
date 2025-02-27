144602606686faaebb449c8b666c240ea32b7b77
Han Kang <58686114+27kanghan@users.noreply.github.com>
2025-02-27 23:17:26 +0900

    Title: 중복되지 않는 정수 중 최대, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 250227/중복되지 않는 정수 중 최대/max-of-unique-number.py
+++ 250227/중복되지 않는 정수 중 최대/max-of-unique-number.py
@@ -2,14 +2,14 @@ n = int(input());
 
 arr = list(map(int, input().split()));
 
-li = [0] * 1000;
+li = [0] * 1001;
 
 for i in arr :
     li[i] += 1;
 
 max_value = -1;
 
-for i in range(999, 0, -1) :
+for i in range(1000, 0, -1) :
     if li[i] == 1 :
         max_value = max(max_value, i);
 
