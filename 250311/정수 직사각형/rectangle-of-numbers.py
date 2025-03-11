a0e892e5ac852541fef1a18a0e4522d9301ff8c3
Han Kang <58686114+27kanghan@users.noreply.github.com>
2025-03-11 18:27:38 +0900

    Title: 정수 직사각형, Time: 61ms, Memory: 17MB, Status: Passed - CodeTree

--- 250311/정수 직사각형/rectangle-of-numbers.py
+++ 250311/정수 직사각형/rectangle-of-numbers.py
@@ -1,20 +1,20 @@
 arr = list(map(int, input().split()))
 
-m = arr[0];
-n = arr[1];
+n = arr[0];
+m = arr[1];
 
 new_arr = [
-    [0 for _ in range (m+1)]
-    for _ in range (n+1)
+    [0 for _ in range (m)]
+    for _ in range (n)
 ]
 
 cnt = 1;
-for i in range (m) :
-    for j in range (n) :
+for i in range (n) :
+    for j in range (m) :
         new_arr[i][j] = cnt;
         cnt+=1
 
-for i in range (m) :
-    for j in range (n) :
+for i in range (n) :
+    for j in range (m) :
         print(new_arr[i][j], end = ' ');
     print();
