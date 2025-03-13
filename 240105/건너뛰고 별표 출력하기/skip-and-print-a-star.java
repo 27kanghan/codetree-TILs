4deeec4f3915de2781223af1c3b0cd3e934d541e
Han Kang <58686114+27kanghan@users.noreply.github.com>
2025-03-13 22:40:21 +0900

    Update skip-and-print-a-star.java

--- 240105/건너뛰고 별표 출력하기/skip-and-print-a-star.java
+++ 240105/건너뛰고 별표 출력하기/skip-and-print-a-star.java
@@ -8,7 +8,6 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
 
-
         int n = Integer.parseInt(st.nextToken());
         for(int i = 0; i < n; i++){
             for(int j = 0; j <= i; j++){
