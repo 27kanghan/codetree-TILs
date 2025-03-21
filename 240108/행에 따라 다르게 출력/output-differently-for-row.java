a3cfbca7d1dac75e28f52e6225e7e32276efca11
Han Kang <58686114+27kanghan@users.noreply.github.com>
2025-03-21 23:38:51 +0900

    Update output-differently-for-row.java

--- 240108/행에 따라 다르게 출력/output-differently-for-row.java
+++ 240108/행에 따라 다르게 출력/output-differently-for-row.java
@@ -8,7 +8,6 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
 
-        
         int n = Integer.parseInt(st.nextToken());
         int cnt = 0;
         for (int i = 0; i < n; i++) {
