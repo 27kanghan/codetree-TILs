49ee589f44591cb051117afe9b36e4417442e8f4
Han Kang <58686114+27kanghan@users.noreply.github.com>
2025-03-25 02:34:22 +0900

    Title: 파스칼의 삼각형, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 250325/파스칼의 삼각형/pascals-triangle.py
+++ 250325/파스칼의 삼각형/pascals-triangle.py
@@ -6,13 +6,13 @@ arr_2d = [
     for _ in range(n)
 ]
 
-for i in range (6) :
+for i in range (n) :
     arr_2d[i][0] = 1
     arr_2d[i][i] = 1
 
 
 cnt = 1;
-for i in range (1, 6) :
+for i in range (1, n) :
     for j in range (1, cnt) :
         arr_2d[i][j] = arr_2d[i-1][j-1] + arr_2d[i-1][j];
     cnt+=1
