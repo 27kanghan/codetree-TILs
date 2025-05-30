60cca40a14f35f7e60540c74c7eb5ac50d037fa4
Han Kang <58686114+27kanghan@users.noreply.github.com>
2025-05-30 22:54:20 +0900

    Title: 정수만 추출하기, Time: 42ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250530/정수만 추출하기/extract-only-integers.py
@@ -0,0 +1,21 @@
+num1, num2 = input().split();
+
+str1 = '';
+str2 = '';
+
+for i in range(0, len(num1)):
+    if num1[i].isdigit() :
+        str1 += num1[i]
+    else :
+        break;
+
+for i in range(0, len(num1)):
+    if num2[i].isdigit() :
+        str2 += num2[i]
+    else :
+        break;
+
+
+print(int(str1) + int(str2))
+
+
