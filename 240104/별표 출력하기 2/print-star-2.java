36bbefa7c346fcb3df0770e7315860128fba5a72
Han Kang <58686114+27kanghan@users.noreply.github.com>
2025-06-03 23:31:30 +0900

    Update print-star-2.java

--- 240104/별표 출력하기 2/print-star-2.java
+++ 240104/별표 출력하기 2/print-star-2.java
@@ -9,7 +9,6 @@ public class Main {
         StringBuilder sb = new StringBuilder();
 
         int n = Integer.parseInt(st.nextToken());
-
         
         for(int i = 0; i < n; i++){
             for(int j = n-i; j > 0; j--){
