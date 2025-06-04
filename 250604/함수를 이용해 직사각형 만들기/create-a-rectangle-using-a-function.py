253516b0c49421360704ba44d6690c2fabce955a
Han Kang <58686114+27kanghan@users.noreply.github.com>
2025-06-04 18:49:19 +0900

    Title: 함수를 이용해 직사각형 만들기, Time: 57ms, Memory: 17MB, Status: Passed - Codetree

---
+++ 250604/함수를 이용해 직사각형 만들기/create-a-rectangle-using-a-function.py
@@ -0,0 +1,11 @@
+n, m = map(int, input().split())
+
+# Please write your code here.
+
+def make_squre(n,m) :
+    for i in range (0,n) :
+        for j in range(0,m):
+            print(1, end='');
+        print();
+        
+make_squre(n,m);
