41c6601cdaf987c8a6da1c703104004d3c1dc128
Han Kang <58686114+27kanghan@users.noreply.github.com>
2025-06-10 20:39:24 +0900

    Title: 함수를 이용한 윤년 판별, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250610/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
@@ -0,0 +1,18 @@
+y = int(input())
+
+# Please write your code here.
+def valid (year) :
+
+    # 4로 떨어지는 해
+    # 100으로 나누어 떨어지지만 400으로 나누어 떨어지지 않으면 평년
+
+    if year % 4 == 0 :
+        if year % 100 == 0 and year % 400 != 0 :
+            return False;
+        return True;
+    
+   
+
+    return True;
+
+print("true" if valid(y) else "false" )
