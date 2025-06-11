e74b1f91ca6d0d1dbe339bf1e889394eb918adb1
Han Kang <58686114+27kanghan@users.noreply.github.com>
2025-06-12 02:26:13 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250612/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
+++ 250612/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
@@ -5,7 +5,10 @@ b = list(map(str, input().split()));
 string1 = "".join(a);
 string2 = "".join(b);
 
-if string1.find(string2) == 1 :
+# print(string1, string2)
+# print(string1 in (string2))
+
+if string2 in string1 :
     print("Yes")
 else :
     print("No")
