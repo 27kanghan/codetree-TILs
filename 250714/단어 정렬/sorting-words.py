4b955602e7867e30ff89b2147de29d6a6c7079d4
Han Kang <58686114+27kanghan@users.noreply.github.com>
2025-07-14 18:18:03 +0900

    Title: 단어 정렬, Time: 53ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250714/단어 정렬/sorting-words.py
@@ -0,0 +1,12 @@
+n = int(input())
+word = [input() for _ in range(n)]
+
+# Please write your code here.
+
+word = sorted(word);
+
+# sorted_arr = ''.join(sorted(word));
+
+
+for elem in word :
+    print(elem)
