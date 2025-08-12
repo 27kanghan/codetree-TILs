1dbede9e7b946f72c9a70d564c3f375cad7ff94a
Han Kang <58686114+27kanghan@users.noreply.github.com>
2025-08-12 22:36:16 +0900

    Title: 국영수 순이지, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250812/국영수 순이지/korean-english-math-order.py
@@ -0,0 +1,23 @@
+class Students:
+    def __init__ (self, name, kor, eng, mat) :
+        self.name = name;
+        self.kor = kor;
+        self.eng = eng;
+        self.mat = mat;
+
+
+n = int(input());
+
+students = [];
+
+for _ in range(n) :
+    name, kor, eng, mat = tuple(input().split());
+
+    students.append(Students(name, int(kor), int(eng), int(mat)));
+
+
+students.sort(key = lambda x : (-x.kor, -x.eng, -x.mat));
+
+for student in students :
+    print(student.name, student.kor, student.eng, student.mat);
+
