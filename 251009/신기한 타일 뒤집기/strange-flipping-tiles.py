88a4040c675442c98dfcbd508aee445b0328dfe7
Han Kang <58686114+27kanghan@users.noreply.github.com>
2025-10-09 01:13:10 +0900

    Title: 신기한 타일 뒤집기, Time: 83ms, Memory: 20MB, Status: Wrong Answer - Codetree

---
+++ 251009/신기한 타일 뒤집기/strange-flipping-tiles.py
@@ -0,0 +1,34 @@
+n = int(input())
+
+max_k = 100000;
+
+arr = [0] * (2 * max_k + 1)
+
+w = 0;
+b = 0;
+
+
+cur_x = max_k
+for i in range(n) :
+    x, direction = input().split()
+    x = int(x)
+
+    if direction == "R" :
+        for j in range(cur_x, cur_x + x) :
+            arr[j] = 1;
+        cur_x = cur_x+x-1
+    
+    else :
+        for j in range (cur_x, cur_x - x, -1) :
+            arr[j] = 2;
+        cur_x = cur_x - x- 1;
+ 
+
+
+for i in range(0, len(arr)) :
+    if arr[i] == 1 :
+        b +=1;
+    elif arr[i] == 2:
+        w += 1;
+
+print(w,b)
