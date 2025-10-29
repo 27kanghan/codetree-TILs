4e2df62cff2a1de4ad034edd05722921f8b069d1
Han Kang <58686114+27kanghan@users.noreply.github.com>
2025-10-30 00:13:10 +0900

    Title: 겹치지 않는 사각형의 넓이, Time: 137ms, Memory: 26MB, Status: Runtime Error - Codetree

---
+++ 251030/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // Please write your code here.
+
+        int offset = 2000;
+
+        int arr [][] = new int[offset][offset];
+
+        for(int i = 0; i < 3; i++){
+            int x1 = sc.nextInt();
+            int y1 = sc.nextInt();
+            int x2 = sc.nextInt();
+            int y2 = sc.nextInt();
+
+            for(int c = x1; c < x2; c++) {
+
+                for(int r = y1; r < y2; r++){
+
+                    if( i != 2 ) {
+                        arr[c][r] = 1;
+                    }
+                    else {
+                        arr[c][r] = 0;
+                    }
+                }
+
+            }
+
+        }
+
+        int answer = 0;
+
+        for(int i = 0; i < offset; i++){
+            for(int j = 0; j < offset; j++){
+                if(arr[i][j] == 1) answer +=1;
+            }
+        }
+
+        System.out.println(answer);
+
+
+    }
+}
