3a0b964804776c6c25224888e5bf2e53a8c7dc59
Han Kang <58686114+27kanghan@users.noreply.github.com>
2025-12-28 21:37:29 +0900

    Title: 문자에 따른 명령 2, Time: 56ms, Memory: 17MB, Status: Passed - Codetree

--- 251228/문자에 따른 명령 2/text-based-commands2.py
+++ 251228/문자에 따른 명령 2/text-based-commands2.py
@@ -8,13 +8,15 @@ cur_dir = 3;
 command = input();
 
 
-for i in range(0, len(command)-1) :
+for i in range(0, len(command)) :
+    # print(command[i])
     if(command[i] == "L") :
         cur_dir = (cur_dir-1+4) % 4
     elif(command[i] == "R") :
         cur_dir = (cur_dir+1) % 4
-    else :
+    elif(command[i] == "F") :
         x += dx[cur_dir]
         y += dy[cur_dir]
+    # print(cur_dir)
 
 print(x, y)
