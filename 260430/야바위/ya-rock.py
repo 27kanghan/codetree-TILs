07b41171fd8335608805e5b4d22582582b7b110d
Han Kang <58686114+27kanghan@users.noreply.github.com>
2026-04-30 16:46:07 +0900

    Title: 야바위, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260430/야바위/ya-rock.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+command = []
+
+for _ in range(N) :
+    command.append(list(map(int,input().split())))
+
+answer = 0
+
+for i in range(3):
+    cup = [0] * 4
+    point = 0
+    cup[i] = 1
+
+    for a,b,c in command :
+        index = cup[a]
+        cup[a] = cup[b]
+        cup[b] = index
+
+        if cup[c] == 1 :
+            point +=1
+    
+    answer = max(answer, point) 
+
+print(answer)
+
